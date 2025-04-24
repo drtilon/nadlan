aced79f3193b233dd542b3cb82155396182d8c68
drtilon <kigdaniel04@gmail.com>
2025-04-24 12:25:54 +0300

    feat: fixing nginx..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['www.shefaug.com']
+    allowedHosts: ['www.shefaug.com','shefaug.com']
   }
 })
